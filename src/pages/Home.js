803a1afe37a984ccd984e1467b9833578150426d
macourage18 <macourage17@gmail.com>
2024-04-29 23:40:14 +0100

    First Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from '../components/navabar/Navbar'
+import { Section1 } from '../components/home/Section1'
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Section1 />
+    </div>
+  )
+}
