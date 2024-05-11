5417177a9119c9d289e5444014eae72084db333d
Major Pinky <107479804+JosephWagio@users.noreply.github.com>
2024-05-11 14:00:10 +0100

    small touches

--- src/components/navabar/Navbar.jsx
+++ src/components/navabar/Navbar.jsx
@@ -7,11 +7,11 @@ import Logo from '../media/WELEARN png.png';
 import './Navbar.css'
 
 export const Navbar = () => {
-  const handleClose = () => {
-    setToggleMenu((prev) => !prev)
-  }
   const [toggleMenu, setToggleMenu] = useState(false);
-
+  
+  // const handleClose = () => {
+  //   setToggleMenu((prev) => !prev)
+  // }
   return (
     <div className='navbar'>
       <div className="navbar-links_logo">
@@ -28,7 +28,7 @@ export const Navbar = () => {
           <li className='active'><a href="#waitlist">Waitlist</a></li>
         </ul>          
         <div className="navbar-download">
-            <button><Link>Download App</Link></button>
+            <button><Link to="/">Download App</Link></button>
           </div>
         <div className="navbar-menu">
           <CgMenuLeft color='#00262F' fontSize={35} onClick={() => setToggleMenu(true)} />
@@ -38,14 +38,14 @@ export const Navbar = () => {
               <IoCloseCircle color='#00262F' className='container_close' fontSize={50} onClick={() => setToggleMenu(false)} />
               <div className='navbar-menu_container-links'>
                 <ul className="navbar-menu-links">
-                  <li><Link to="/" onClick={handleClose}>Home</Link></li>
-                  <li className='active' onClick={handleClose}><a href="#service">Why Use Welearn</a></li>
-                  <li className='active' onClick={handleClose}><a href="#howitworks">How It Works</a></li>
-                  <li className='active' onClick={handleClose}><a href="#testimonials">Testimonials</a></li>
-                  <li className='active' onClick={handleClose}><a href="#waitlist">Waitlist</a></li>
+                  <li><Link to="/">Home</Link></li>
+                  <li className='active'><a href="#service">Why Use Welearn</a></li>
+                  <li className='active'><a href="#howitworks">How It Works</a></li>
+                  <li className='active'><a href="#testimonials">Testimonials</a></li>
+                  <li className='active'><a href="#waitlist">Waitlist</a></li>
                 </ul>
                 <div className="navbar-menu-download">
-                  <button><Link>Download App</Link></button>
+                  <button><Link to="/">Download App</Link></button>
                 </div>            
               </div>
             </div>
