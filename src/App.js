9d4ccf785df9387a85ed86e7d5949f4d8c5888e0
macourage18 <macourage17@gmail.com>
2024-05-13 20:22:43 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { DashboardPage } from './pages/DashboardPage';
+import { LoginPage } from './pages/LoginPage';
+import { ParentsPage } from './pages/ParentsPage';
+import { TutorsPage } from './pages/TutorsPage';
+import { AdminPage } from './pages/AdminPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path={'/login'} element= {<LoginPage/>} />
+          <Route path={"/dashboard"} element={<DashboardPage/>} />
+          <Route path={'/parents'} element={<ParentsPage/>} />
+          <Route path={'/tutors'} element={<TutorsPage/>} />
+          <Route path={'/admin'} element={<AdminPage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/ParentsPage.js
+++ src/pages/ParentsPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const sidebar = () => {
+export const ParentsPage = () => {
   return (
     <div>
       
