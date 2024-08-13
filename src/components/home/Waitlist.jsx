885b4811e010ac9376afb57c2b0d1974af16d857
Bright Philip <chibuzorphilip2001@gmail.com>
2024-08-13 22:06:39 +0100

    Latest

--- src/components/home/Waitlist.jsx
+++ src/components/home/Waitlist.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import appk from '../media/welearn.apk'
 import './Waitlist.css';
 
 export const Waitlist = () => {
   return (
     <div className='waitlist section__padding' id='waitlist'>
-        <h1>Join our waitlist</h1>
-        <p>Join our waitlist by dropping your name and email to<br/> get further information</p>
-        <button>Submit</button>
+        <h1>Join Welearn</h1>
+        <p>Join us today to hire professional instructors to handle your kids</p>
+        <a href={appk}><button>Download App</button></a>
     </div>
   )
 }
