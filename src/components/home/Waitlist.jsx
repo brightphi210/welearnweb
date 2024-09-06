4254f0208d69de7eb8eb4746c7b2510ac0c13f72
Bright Philip <chibuzorphilip2001@gmail.com>
2024-09-06 17:41:31 +0100

    Latest

--- src/components/home/Waitlist.jsx
+++ src/components/home/Waitlist.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import appk from '../media/welearn.apk'
+import appk2 from '../media/welearnnew.apk'
 import './Waitlist.css';
 
 export const Waitlist = () => {
@@ -7,7 +8,7 @@ export const Waitlist = () => {
     <div className='waitlist section__padding' id='waitlist'>
         <h1>Join Welearn</h1>
         <p>Join us today to hire professional instructors to handle your kids</p>
-        <a href={appk}><button>Download App</button></a>
+        <a href={appk2}><button>Download App</button></a>
     </div>
   )
 }
